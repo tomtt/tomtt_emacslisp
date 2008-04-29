967fb12224803e9d35b3abd72777ac04d1d01694
Tom ten Thij <git@tomtenthij.nl>
2008-04-29 13:38:09 +0100

    Reorganized emacs files to eliminate dependencies on my personal setup and move identity stuff to dedicated file

--- my-snippets.el
+++ my-snippets.el
@@ -1,6 +1,3 @@
-;; my personal preferences
-;;(partial-completion-mode t)
-
 (snippet-with-abbrev-table 'text-mode-abbrev-table
   ("ehtml" .  "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Strict//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">
 <html lang=\"en\">
@@ -108,59 +105,4 @@ end"))
   ("li" . "    %li
 $>$."))
 
-(defun html-indent-or-complete ()
-  "Complete if point is at end of a word, otherwise indent line."
-  (interactive)
-  (if snippet
-      (snippet-next-field)
-    (if (looking-at "\\>")
-        (progn
-          (hippie-expand nil)
-          (indent-for-tab-command))
-      (indent-for-tab-command))))
-
-(require 'sgml-mode)
-(define-key emacs-lisp-mode-map "\C-j" 'eval-last-sexp)
-(define-key html-mode-map "\t" 'html-indent-or-complete)
-
-;(require 'gnuserv)
-;(gnuserv-start)
-;(setq gnuserv-frame (selected-frame))
-;(setenv "GNUSERV_SHOW_EMACS" "1")
-
-(fset 'insert-selection-as-color
-   [?\C-y ?\C-  M-left left ?\C-x ?r ?s ?c ?\C-w M-left M-right ?\C-k ?\C-  M-left ?\C-x ?r ?s ?n ?\C-w ?\C-x ?r ?i ?c M-right ?\; ?  ?\C-c ?\C-c ?\C-x ?r ?i ?n ?\C-e left return])
-(define-key cssm-mode-map "\C-c \C-i" 'insert-selection-as-color)
-(define-key cssm-mode-map "\C-c \C-l" 'list-colors-display)
-
-(defun tomtt-planner-config ()
-  (define-key planner-mode-map "\C-c\C-q" 'planner-task-open)
-  (define-key planner-mode-map "\C-c\C-w" 'planner-task-pending))
-(add-hook 'planner-mode-hook 'tomtt-planner-config)
-
-(defun open-file-as-log (&optional log-file)
-  (interactive)
-  (let* ((log-file (if log-file log-file "c:/tomtt/cygwin/tmp/bla"))
-         (buffer (rails-log:buffer-name log-file))
-         (current (buffer-name)))
-    (unless (get-buffer buffer)
-      (get-buffer-create buffer)
-      (set-buffer buffer)
-      (setq auto-window-vscroll t)
-      (setq buffer-read-only t)
-      (set-buffer current)
-      (apply-colorize-to-buffer buffer))
-    (start-process "tail"
-                   buffer
-                   "tail"
-                   "-n 130"
-                   "-f" log-file)))
-
-(define-keys rails-minor-mode-map
-  ((rails-key "\C-c i s") 'it-start)
-  ((rails-key "\C-c i p") 'it-pend)
-  ((rails-key "\C-c i u") 'it-unpend))
-
-
-(provide 'tomtt)
-
+(provide 'my-snippets)
---
+++ scratch.el
@@ -0,0 +1,58 @@
+;; Where random and experimental crap lives
+
+(defun html-indent-or-complete ()
+  "Complete if point is at end of a word, otherwise indent line."
+  (interactive)
+  (if snippet
+      (snippet-next-field)
+    (if (looking-at "\\>")
+        (progn
+          (hippie-expand nil)
+          (indent-for-tab-command))
+      (indent-for-tab-command))))
+
+(require 'sgml-mode)
+(define-key emacs-lisp-mode-map "\C-j" 'eval-last-sexp)
+(define-key html-mode-map "\t" 'html-indent-or-complete)
+
+;(require 'gnuserv)
+;(gnuserv-start)
+;(setq gnuserv-frame (selected-frame))
+;(setenv "GNUSERV_SHOW_EMACS" "1")
+
+(fset 'insert-selection-as-color
+   [?\C-y ?\C-  M-left left ?\C-x ?r ?s ?c ?\C-w M-left M-right ?\C-k ?\C-  M-left ?\C-x ?r ?s ?n ?\C-w ?\C-x ?r ?i ?c M-right ?\; ?  ?\C-c ?\C-c ?\C-x ?r ?i ?n ?\C-e left return])
+(define-key cssm-mode-map "\C-c \C-i" 'insert-selection-as-color)
+(define-key cssm-mode-map "\C-c \C-l" 'list-colors-display)
+
+(defun tomtt-planner-config ()
+  (define-key planner-mode-map "\C-c\C-q" 'planner-task-open)
+  (define-key planner-mode-map "\C-c\C-w" 'planner-task-pending))
+(add-hook 'planner-mode-hook 'tomtt-planner-config)
+
+(defun open-file-as-log (&optional log-file)
+  (interactive)
+  (let* ((log-file (if log-file log-file "c:/tomtt/cygwin/tmp/bla"))
+         (buffer (rails-log:buffer-name log-file))
+         (current (buffer-name)))
+    (unless (get-buffer buffer)
+      (get-buffer-create buffer)
+      (set-buffer buffer)
+      (setq auto-window-vscroll t)
+      (setq buffer-read-only t)
+      (set-buffer current)
+      (apply-colorize-to-buffer buffer))
+    (start-process "tail"
+                   buffer
+                   "tail"
+                   "-n 130"
+                   "-f" log-file)))
+
+(define-keys rails-minor-mode-map
+  ((rails-key "\C-c i s") 'it-start)
+  ((rails-key "\C-c i p") 'it-pend)
+  ((rails-key "\C-c i u") 'it-unpend))
+
+;;(partial-completion-mode t)
+
+(provide 'scratch)
