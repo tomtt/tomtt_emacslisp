ab59293df77c5d274b73bfb73ea447ec03273123
Tom ten Thij <git@tomtenthij.nl>
2008-12-13 00:09:26 +0000

    Experimental code to try to handle textmate urls

--- scratch.el
+++ scratch.el
@@ -90,5 +90,37 @@
 (eval-after-load "ffap"
   '(push '(ruby-mode . ruby-module-path) ffap-alist))
 
+(setq dired-recursive-deletes 'top
+  dired-recursive-copies 'top
+  dired-dwim-target t)
+(add-hook 'dired-mode-hook (lambda ()
+                             (local-set-key (kbd "C-c C-r")
+                               'wdired-change-to-wdired-mode)))
+
+(defun mac-ae-get-url (event)
+  "Open the URL specified by the Apple event EVENT.
+Currently the `mailto' and `txmt' schemes are supported."
+  (interactive "e")
+  (let* ((ae (mac-event-ae event))
+         (the-text (mac-ae-text ae))
+         (parsed-url (url-generic-parse-url the-text))
+         (the-url-type (url-type parsed-url)))
+    (case (intern the-url-type)
+      (mailto
+       (progn
+         (url-mailto parsed-url)
+         (select-frame-set-input-focus (selected-frame))))
+      (txmt
+       (let* ((not-used (string-match "txmt://open\\?url=file://\\
+([^&]*\\)\\(&line=\\([0-9]*\\)\\)?" the-text))
+              (file-name (match-string 1 the-text))
+              (lineno (match-string 3 the-text)))
+         (if (null file-name)
+             (message "Bad txmt URL: %s" the-text)
+           (find-file file-name)
+           (goto-line (if lineno (string-to-number lineno) 0))
+           (select-frame-set-input-focus (selected-frame)))))
+      (t (mac-resume-apple-event ae t)))))
+
 (provide 'scratch)
 
